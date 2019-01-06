c337ec6dbb290f6377195172c94ad0edece663a4
lemon-li <lile1@kingsoft.com>
2019-01-06 21:24:41 +0800

    Add dagger2 injection for RouteManager class.

--- src/main/java/com/seasungames/appinhouse/routes/RoutesManager.java
+++ src/main/java/com/seasungames/appinhouse/routes/RoutesManager.java
@@ -1,11 +1,7 @@
 package com.seasungames.appinhouse.routes;
 
 import com.seasungames.appinhouse.application.APIConstant;
-import com.seasungames.appinhouse.services.impl.AppServiceImpl;
-import com.seasungames.appinhouse.services.impl.VersionServiceImpl;
-import com.seasungames.appinhouse.stores.dynamodb.DynamoDBManager;
 import com.seasungames.appinhouse.utils.PathUtils;
-import io.vertx.core.Vertx;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
@@ -14,7 +10,8 @@ import com.seasungames.appinhouse.routes.handlers.RouteAppHandler;
 import com.seasungames.appinhouse.routes.handlers.RouteVersionHandler;
 
 import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.handler.BodyHandler;
+
+import javax.inject.Inject;
 
 /**
  * Created by lile on 12/27/2018
@@ -23,21 +20,17 @@ public class RoutesManager {
 
     private static final Logger log = LoggerFactory.getLogger(RoutesManager.class);
 
-    private final Router router;
+    @Inject
+    Router router;
 
-    private static final String API_APPS = "/api/apps";
-    private static final String API_VERSIONS = "/api/versions";
+    @Inject
+    RouteAppHandler appHandler;
 
-    DynamoDBManager dbManager;
-    private AppServiceImpl appService;
-    private VersionServiceImpl versionService;
+    @Inject
+    RouteVersionHandler versionHandler;
 
-    public RoutesManager(Vertx vertx, DynamoDBManager dbManager) {
-        this.router = Router.router(vertx);
-        this.dbManager = dbManager;
+    public RoutesManager() {
 
-        appService = new AppServiceImpl(this.dbManager.appTable);
-        versionService = new VersionServiceImpl(this.dbManager.versionTable);
     }
 
     public Router getRouter() {
@@ -45,10 +38,6 @@ public class RoutesManager {
     }
 
     public RoutesManager setRoutes() {
-        RouteAppHandler appHandler = new RouteAppHandler(appService);
-        RouteVersionHandler versionHandler = new RouteVersionHandler(versionService);
-
-        //webroot
         router.route(APIConstant.INDEX).handler(this::index);
         router.route(APIConstant.API_INDEX_APP).handler(appHandler::index);
         router.route(APIConstant.API_INDEX_VERSION).handler(versionHandler::index);
