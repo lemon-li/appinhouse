99b873c9ffddd5cb1a3224f91272a7d0e693b91c
lemon-li <lile1@kingsoft.com>
2019-01-09 20:14:37 +0800

    Add common json result process clsss.

---
+++ src/main/java/com/seasungames/appinhouse/models/ResponseVo.java
@@ -0,0 +1,45 @@
+package com.seasungames.appinhouse.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.vertx.core.json.Json;
+
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+public class ResponseVo<T> {
+
+    private T data;
+
+    private String message;
+
+    private int code;
+
+    public ResponseVo setData(T data) {
+        this.data = data;
+        return this;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public ResponseVo setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public ResponseVo setCode(int code) {
+        this.code = code;
+        return this;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String toJson() {
+        return Json.encode(this).toString();
+    }
+}
