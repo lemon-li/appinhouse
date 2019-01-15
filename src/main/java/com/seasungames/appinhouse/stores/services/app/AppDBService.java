ccc3a76db9eb431800cf10dde38d2559f03e18af
lemon-li <lile1@kingsoft.com>
2019-01-15 13:35:14 +0800

    Add appDBservice and versionDBservice.

--- src/main/java/com/seasungames/appinhouse/stores/services/app/AppDBService.java
+++ src/main/java/com/seasungames/appinhouse/stores/services/app/AppDBService.java
@@ -1,25 +1,20 @@
-package com.seasungames.appinhouse.stores.services;
+package com.seasungames.appinhouse.stores.services.app;
 
 import com.seasungames.appinhouse.models.AppVo;
-import com.seasungames.appinhouse.models.VersionVo;
 import com.seasungames.appinhouse.models.response.AppListResponseVo;
 import com.seasungames.appinhouse.models.response.AppResponseVo;
-import com.seasungames.appinhouse.models.response.VersionResponseVo;
 import io.vertx.codegen.annotations.ProxyClose;
 import io.vertx.codegen.annotations.ProxyGen;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 
-import java.util.List;
-
 /**
- * Created by lile on 1/14/2019
+ * Created by lile on 1/15/2019
  */
-
 @ProxyGen
-public interface DynamoDBService {
+public interface AppDBService {
 
-    String SERVICE_ADDRESS = "com.seasungames.appinhouse.dynamodb-service";
+    String SERVICE_ADDRESS = "com.seasungames.appinhouse.app-dynamodb-service";
 
     void getAppsList(String lastKey, Handler<AsyncResult<AppListResponseVo>> resultHandler);
 
@@ -30,14 +25,6 @@ public interface DynamoDBService {
     void deleteApps(String appId, Handler<AsyncResult<Void>> resultHandler);
 
     void getApps(String appId, Handler<AsyncResult<AppResponseVo>> resultHandler);
-
-    void getPlatformList(String appId, String platform, Handler<AsyncResult<List<VersionResponseVo>>> resultHandler);
-
-    void getLatestList(String appId, Handler<AsyncResult<List<VersionResponseVo>>> resultHandler);
-
-    void createVersion(VersionVo vo, Handler<AsyncResult<Void>> resultHandler);
-
-    void getVersion(String appId, String platform, String version, Handler<AsyncResult<VersionVo>> resultHandler);
 
     @ProxyClose
     void close(Handler<AsyncResult<Void>> resultHandler);
